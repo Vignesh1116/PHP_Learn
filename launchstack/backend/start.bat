41a9b36d61208918e24be39107ce36644a57f60c
Vignesh1116 <vigneshbalan1116@gmail.com>
2026-05-27 20:15:59 +0530

    Add start.bat and composer.phar

---
+++ launchstack/backend/start.bat
@@ -0,0 +1,4 @@
+@echo off
+set PATH=%PATH%;C:\Users\SANTHOSH\AppData\Local\Microsoft\WinGet\Packages\PHP.PHP.8.2_Microsoft.Winget.Source_8wekyb3d8bbwe
+php composer.phar install
+echo Dependencies installed!
